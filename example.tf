2be278f33c53d8a35b68edef5ef0bfe6c76b4668
SunilCheema <sunil03cheema@hotmail.co.uk>
2019-07-31 14:20:51 +0100

    Update example.tf

--- example.tf
+++ example.tf
@@ -43,7 +43,7 @@ tags = {
 }
 
 resource "aws_route_table" "public" {
-  vpc_id = "${aws_vpc.default.id}"
+  vpc_id = "${aws_vpc.terraform2.id}"
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -56,7 +56,7 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table" "private" {
-  vpc_id = "${aws_vpc.default.id}"
+  vpc_id = "${aws_vpc.terraform2.id}"
 
   route {
     cidr_block = "0.0.0.0/0"
