f8bd32a1c958c92fe1203e7076739dc45913b29a
SunilCheema <sunil03cheema@hotmail.co.uk>
2019-07-31 14:14:44 +0100

    Update example.tf

--- example.tf
+++ example.tf
@@ -14,6 +14,14 @@ tags = {
 
 }
 
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.terraform2.id}"
+
+  tags = {
+    Name = "main"
+  }
+}
+
 resource "aws_subnet" "public_subnet" {
   
 vpc_id     = "${aws_vpc.terraform2.id}"
@@ -32,6 +40,37 @@ tags = {
     Name = "terraformWork"
   }
 
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = "${aws_vpc.default.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.main.id}"
+  }
+
+  tags = {
+    Name = "terraformWork"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = "${aws_vpc.default.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.main.id}"
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    egress_only_gateway_id = "${aws_egress_only_internet_gateway.foo.id}"
+  }
+
+  tags = {
+    Name = "main"
+  }
 }
 
 resource "aws_instance" "example" {
