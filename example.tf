aeafbdb7eb8e79999adda793a6a08aa99a2d4ddc
SunilCheema <sunil03cheema@hotmail.co.uk>
2019-07-31 12:28:15 +0100

    Update example.tf

--- example.tf
+++ example.tf
@@ -1,12 +1,9 @@
 provider "aws" {
   
 profile    = "default"
-  
 region     = "us-east-1"
 
 }
-
-
 
 resource "aws_vpc" "terraform2" {
 
@@ -19,44 +16,30 @@ tags = {
 
 }
 
-
-
 resource "aws_subnet" "public_subnet" {
   
 vpc_id     = "${aws_vpc.terraform2.id}"
-  
 cidr_block = "10.0.1.0/24"
-
-  
 tags = {
     Name = "terraformWork"
   }
 
 }
 
-
-
-resource "aws_subnet" "private_subnet"
- {
+resource "aws_subnet" "private_subnet" {
 vpc_id     = "${aws_vpc.terraform2.id}"
-  
 cidr_block = "10.0.1.0/24"
-
   
 tags = {
     Name = "terraformWork"
   }
 
 }
-
-
 
 resource "aws_instance" "example" {
   
 ami           = "ami-b374d5a5"
-  
 instance_type = "t2.micro"
-  
 subnet_id = "${aws_subnet.private_subnet.id}"
 
 }
